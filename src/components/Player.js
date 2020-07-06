b618beac3f23369a6d0421df93653f956fe59b72
Akhaled19 <56312360+Akhaled19@users.noreply.github.com>
2020-07-06 15:19:21 -0400

    add update code to Player component to use Redux. Wire up my event handler to the proper action creator method

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,16 +1,19 @@
 import React, {PropTypes} from 'react';
-
 import Counter from './Counter';
 
 const Player = props => {
     return (
       <div className="player">
         <div className="player-name">
-          <a className="remove-player" onClick={props.onRemove}>✖</a>
+          <a className="remove-player" onClick={ () => props.removePlayer(props.index) }>✖</a>
           {props.name}
         </div>
         <div className="player-score">
-          <Counter onChange={props.onScoreChange} score={props.score} />
+          <Counter 
+          index={props.index} 
+          updatePlayerScore={props.updatePlayerScore}
+          score={props.score} 
+          />
         </div>
       </div>
     );
