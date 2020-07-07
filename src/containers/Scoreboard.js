2e25e82d9ff742af214723195b3059d701934264
Akhaled19 <56312360+Akhaled19@users.noreply.github.com>
2020-07-07 01:38:36 -0400

    updated scoreboard with new changes

--- src/containers/Scoreboard.js
+++ src/containers/Scoreboard.js
@@ -5,16 +5,23 @@ import AddPlayerForm from '../components/AddPlayerForm';
 import * as PlayerActionCreators from '../actions/player';
 import Player from '../components/Player';
 import Header from '../components/Header';
+import PlayerDetail from '../components/PlayerDetail';
 
 
 class Scoreboard extends Component {
 
   render() {
-    const { dispatch, players } = this.props;
+    const { dispatch, players, selectedPlayerIndex } = this.props;
     const addPlayer = bindActionCreators(PlayerActionCreators.addPlayer, dispatch);
     const removePlayer = bindActionCreators(PlayerActionCreators.removePlayer, dispatch);
     const updatePlayerScore = bindActionCreators(PlayerActionCreators.updatePlayerScore, dispatch);
-    
+    const selectPlayer = bindActionCreators(PlayerActionCreators.selectPlayer, dispatch);
+
+    let selectedPlayer;
+    if(selectedPlayerIndex !== -1) {
+      selectedPlayer = players[selectedPlayerIndex];
+    }
+
     const playerComponents = players.map((player, index) => (
       <Player 
         index={index}
@@ -23,8 +30,10 @@ class Scoreboard extends Component {
         key={player.name}
         updatePlayerScore={updatePlayerScore}
         removePlayer={removePlayer}
+        selectPlayer={selectPlayer}
       />
     ));
+    
     return (
       <div className="scoreboard">
         <Header players={players} />
@@ -32,6 +41,9 @@ class Scoreboard extends Component {
           { playerComponents }
         </div>
         <AddPlayerForm addPlayer={addPlayer} />
+        <div className='player-detail'>
+          <PlayerDetail selectedPlayer={selectedPlayer} />
+        </div>
       </div>
     );
   }
@@ -39,7 +51,8 @@ class Scoreboard extends Component {
 
 const mapStateToProps = state => (
   {
-    players: state
+    players: state,
+    selectedPlayerIndex: state.selectedPlayerIndex
   }
 );
 
